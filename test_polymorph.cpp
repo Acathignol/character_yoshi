8af56b31a0f2723c051a79354c3ff21d24415842
Annie Cathignol <annie.cathignol@insa-lyon.fr>
2016-01-25 16:34:31 +0100

    Creation of a document.txt for Acceleration / Breaks

--- test_polymorph.cpp
+++ test_polymorph.cpp
@@ -4,7 +4,12 @@
 // ===========================================================================
 #include <cstdio>
 #include <cstdlib>
-#
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 #include "Character.h"
 #include "Yoshi.h"
 #include "Mario.h"
@@ -24,29 +29,34 @@ int main(int argc, char* argv[]) {
   Mario       character3 = Mario(0,Mario::BLUE);
   Mario       character4 = Mario(2,Mario::RED);
 
-  character1.Accelerate();
-  character2.Accelerate();
-  character3.Accelerate();
-  character4.Accelerate();
+  ofstream fichier("Speed.txt", ios::out | ios::trunc);  // ouverture en écriture avec effacement du fichier ouvert
+  if (fichier) {
+    for (int i=0; i<11; i++){
+      character1.Accelerate();
+      character2.Accelerate();
+      character3.Accelerate();
+      character4.Accelerate();
+      fichier << "Char1 : " << character1.speed()<< " - Char2 : " << character2.speed()<< " - Char3 : " << character3.speed()<<" - Char4 : " << character4.speed()<< endl;
+    }
+    for (int i=0; i<11; i++){
+      character1.Break();
+      character2.Break();
+      character3.Break();
+      character4.Break();
+      fichier << "Char1 : " << character1.speed()<< " - Char2 : " << character2.speed()<< " - Char3 : " << character3.speed()<<" - Char4 : " << character4.speed()<< endl;
+    }
+    fichier.close();
+  }
+  else
+    cerr << "Impossible d'ouvrir le fichier !" << endl;
+
 
   printf("Character 1 speed : %f\n", character1.speed());
   printf("Character 2 speed : %f\n", character2.speed());
   printf("Character 3 speed : %f\n", character3.speed());
   printf("Character 4 speed : %f\n", character4.speed());
+  printf("\n" );  
 
-
-  printf("\n" );
-
-  character1.Break();
-  character2.Break();
-  character3.Break();
-  character4.Break();
-
-  printf("Character 1 speed : %f\n", character1.speed());
-  printf("Character 2 speed : %f\n", character2.speed());
-  printf("Character 3 speed : %f\n", character3.speed());
-  printf("Character 4 speed : %f\n", character4.speed());
-  
   character1.WhatAmI();
   character2.WhatAmI();
 
